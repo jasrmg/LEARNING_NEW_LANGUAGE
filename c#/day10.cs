a58013664d238a060eb1414f03c32d6c2c91b46c
jeff <jeff@gmail.com>
2025-04-22 17:18:30 +0800

    finished day10

---
+++ c#/day10.cs
@@ -0,0 +1,5 @@
+
+string str1 = "Hello World";
+char[] uniqueArr = str1.ToCharArray().Distinct().ToArray();
+string str2 = string.Join("", uniqueArr);
+Console.WriteLine(str2);
--- c#/day9.cs
+++ c#/day9.cs
@@ -1,6 +1,6 @@
 string str1 = "the quick brown fox";
 char[] arr1 = str1.ToCharArray();
-Console.WriteLine(arr1);
+// Console.WriteLine(arr1);
 
 for (int i = 0; i < arr1.Length; i++) {
   Console.WriteLine(arr1[i]);
