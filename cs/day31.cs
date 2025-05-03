7b42ce6db7e3e6001a98cde01d35b63c3f79c1a7
jeff <jeff@gmail.com>
2025-05-03 08:51:53 +0800

    finished day31

---
+++ cs/day31.cs
@@ -0,0 +1,15 @@
+Dictionary<int, string> dict = new Dictionary<int, string> {
+  {1, "One"},
+  {2, "Two"},
+  {3, "Three"},
+  {4, "Four"},
+  {5, "Five"},
+  {6, "Six"},
+  {7, "Seven"},
+  {8, "Eight"},
+  {9, "Nine"},
+  {10, "Ten"},
+};
+for (int i = 1; i <= 10; i++) {
+  Console.WriteLine(dict[i]);
+}
